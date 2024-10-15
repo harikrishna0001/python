dce9fad94038944aca5c86a410b76143bdf6bcb6
harikrishna0001 <nharikrishnanm@gmail.com>
2024-10-15 12:12:40 +0530

    updated

---
+++ pattern.py
@@ -0,0 +1,83 @@
+# # for i in range(3):
+# #     for j in range(1,4):
+# #       print(j,end="")
+# #     print()
+
+# 123
+# 123
+# 123
+    
+# num=1   
+# for i in range(1,4):
+#     for j in range(1,4):
+#       print(num,end=" ")
+#       num+=1
+#     print()
+    
+# 1 2 3 
+# 4 5 6 
+# 7 8 9 
+    
+# for i in range(1,4):
+#     num=i  
+#     for j in range(1,4):
+#         print(num,end=" ")
+#         num+=1
+#     print()
+# 1 2 3 
+# 2 3 4 
+# 3 4 5 
+ 
+# for i in range(1,4):
+#     num=i  
+#     for j in range(i):
+#        print('*',end=" ")
+#        num+=1
+#     print()   
+# 1 
+# 2 3 
+# 3 4 5 
+
+# for i in range(3,0,-1): 
+#     for j in range(i):
+#        print('*',end=" ")
+#     print()   
+    
+# * * * 
+# * * 
+# * 
+
+# for i in range(1,5): 
+#     for j in range(5,0,-1):
+#         if j>i:
+#            print(" ",end=" ")
+#         else:
+#           print('*',end=" ")
+#     print()  
+    
+#         * 
+#       * * 
+#     * * * 
+#   * * * * 
+
+# for i in range(1,4):
+#     for j in range(1,4):
+#         if j<=i:
+#             print(j,end=" ")
+    
+#     print()   
+    
+# 1 
+# 1 2 
+# 1 2 3 
+    
+# a=65
+# for  i in range(1,4):
+#     for j in range(1,4):
+#         print(chr(a),end="")
+#         a+=1
+#     print()
+    
+# ABC
+# DEF
+# GHI
