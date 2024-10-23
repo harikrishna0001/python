70d141adae7f8dd7515583490de80a0249cb0fa5
harikrishna0001 <nharikrishnanm@gmail.com>
2024-10-23 11:38:28 +0530

    updated

---
+++ fuctionarguments.py
@@ -0,0 +1,11 @@
+# def std(name,age):
+#     print(name,age)
+# std(22,'shino')
+
+# 22 shino
+
+# def std(name,age):
+#     print(name,age)
+# std(age=20,name='riju')
+
+# riju 20
---
+++ function.py
@@ -0,0 +1,25 @@
+# def sum():
+#     a=int(input("enter the first number"))
+#     b=int(input("enter the second number"))
+#     print(a+b)
+# sum()  
+
+
+
+# def sum():
+#     a=int(input("enter the first number"))
+#     b=int(input("enter the second number"))  using return
+#     c=a+b
+#     return c
+# sum=sum()
+# print(sum)
+
+
+
+def sum(a,b):
+    c=a+b
+    return c
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+c=sum(a,b)
+print(c)
