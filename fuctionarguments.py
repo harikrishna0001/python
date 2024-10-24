273c2c117c6d1a6f7e13deb71aaf3435fb422d62
harikrishna0001 <nharikrishnanm@gmail.com>
2024-10-24 11:43:14 +0530

    updated

--- fuctionarguments.py
+++ fuctionarguments.py
@@ -9,3 +9,40 @@
 # std(age=20,name='riju')
 
 # riju 20
+
+
+# function with arbitary
+
+# def std(*arg):
+#    print(arg)
+# std('manu','22','kakkanad')
+# std('gokul','21')
+# std()
+# std(10)
+
+# ('manu', '22', 'kakkanad')
+# ('gokul', '21')
+# ()
+# (10,)
+
+# fun with default parameter value
+
+# def std(name,age,course='java'):
+#     print(name,age,course)        setting default name(java)
+# std('manu',22,'python')
+# std('hari',23)
+
+# manu 22 python
+# hari 23 java
+
+
+# def std(**arg):
+#     print(arg)
+# std(name='manu',age=22,course='python')  out will be dictinary
+# std(name='john',age=21)
+# std(name='appu')
+
+# {'name': 'manu', 'age': 22, 'course': 'python'}
+# {'name': 'john', 'age': 21}
+# {'name': 'appu'}
+
---
+++ lambda.py
@@ -0,0 +1,7 @@
+# add=lambda a,b:a+b
+# print(add(5,4))
+
+# def add(a,b):
+#     print(a+b)
+# add(5,7)
+
