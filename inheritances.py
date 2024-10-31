a0ab24846e81acb90d324995bf4c4595c291689a
harikrishna0001 <nharikrishnanm@gmail.com>
2024-10-31 11:32:13 +0530

    updated

---
+++ inheritances.py
@@ -0,0 +1,61 @@
+# single inheritance
+
+# class school:
+#     def notes():
+#          print("n")
+#     def teacher(): 
+#          print("t")
+# class student(school):
+#     def house():
+#       print("house")
+# std1=student
+# std1.notes()
+# std1.teacher()
+# std1.house()
+
+# multiple inheritance
+
+# class school:
+#     def notes():
+#          print("css")
+#     def teacher(): 
+#          print("priya")
+# class tution:
+#     def tnotes():
+#          print("cc1")
+# class student(school,tution):
+#          print("rh")
+# std1=student
+# std1.notes()
+# std1.teacher()
+
+# multi level inheritance
+
+# class school:
+#     def ten():
+#       print("a,b,c")
+# class class_room(school):
+#     def a():
+#       print("20 std")
+# class student(teacher):
+#     def rahul():
+#       print("good")
+# std1=student()
+# std1.ten()
+# std1.a()
+
+# hiraracieal inheritance
+
+# class school:
+#   def high():
+#     print("1,2,3,4")
+# class student(school):
+#   def roy():
+#     print("good student")
+# class teacher(school):
+#   def michel():
+#     print("friendly")
+    
+# obj1=student()
+# obj1.high
+# obj2=teacher()
